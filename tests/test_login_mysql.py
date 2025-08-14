62ac42e37cdc8871abc812da579082f4b7a7c995
FayHosseini-GLI <a.hosseini@gaminglabs.com>
2025-08-15 07:56:01 +1000

    Fix CI import error by setting PYTHONPATH in GitHub Actions workflow

--- tests/test_login_mysql.py
+++ tests/test_login_mysql.py
@@ -7,12 +7,19 @@ def test_login(driver, login_data):
     for username, password in login_data:
         driver.get("https://the-internet.herokuapp.com/login")
 
-        wait = WebDriverWait(driver, 10)
-        wait.until(EC.presence_of_element_located((By.ID, "username"))).send_keys(username)
-        wait.until(EC.presence_of_element_located((By.ID, "password"))).send_keys(password)
+        wait = WebDriverWait(driver, 20)
+        username_field = wait.until(EC.visibility_of_element_located((By.ID, "username")))
+        username_field.clear()
+        username_field.send_keys(username)
+
+        password_field = wait.until(EC.visibility_of_element_located((By.ID, "password")))
+        password_field.clear()
+        password_field.send_keys(password)
+
         wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
 
-        # Example assertion
+               # Example assertion
         assert "secure" in driver.current_url or "error" in driver.page_source
+
 
 
